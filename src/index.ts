7aeafcd0b59e718fec5a09f638ef90898f026a8e
Lee Norris <lee.norris@arcadia.io>
2021-02-18 09:32:58 -0500

    chore: add imports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { BrowserService } from './lib/services/browser.service';
 export { driverToken } from './lib/tokens/driver-token';
 export { RenderService } from './lib/services/render.service';
 export { ServerBrowserModule } from './lib/server-browser.module';
+import './lib/test';
